ca189920f298679910b64408a556a7a2f9b6aee9
ComeOnBabiii <duccc0926@gmail.com>
2020-08-18 10:58:23 +0700

    user

--- src/main/java/com/ddq/admin/controller/UserController.java
+++ src/main/java/com/ddq/admin/controller/UserController.java
@@ -1,28 +1,25 @@
-package com.ddq.controller;
+package com.ddq.admin.controller;
 
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ddq.dao.UserDao;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
-public class HelloWorldController {
-
+@RequestMapping("/admin")
+public class UserController {
 	@Autowired
 	UserDao userDao;
-	
-	@RequestMapping({ "/hello" })
-	public String firstPage() {
-		return "Hello World";
-	}
 
-	@GetMapping("/getUser")
-	public List<Map<String,Object>> getUsers() {
+	@GetMapping("/getUsers")
+	public List<Map<String, Object>> getUsers() {
 		return userDao.findAll();
 	}
 }
